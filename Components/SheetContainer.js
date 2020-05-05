9e0cb0ee2e1ffa46a1b5420a3b0cc694c18b1f72
ThomasShih <55861476+ThomasShih@users.noreply.github.com>
2020-05-04 21:41:46 -0600

    Slightly Refactoring

--- Components/SheetContainer.js
+++ Components/SheetContainer.js
@@ -3,30 +3,30 @@ import ReactMarkdown from 'react-markdown';
 import MathJax from 'react-mathjax';
 import RemarkMathPlugin from 'remark-math';
 
+//hat tip: https://medium.com/@MatDrinksTea/rendering-markdown-and-latex-in-react-dec355e74119
 function MarkdownRender(props) {
   const newProps = {
-      ...props,
-      plugins: [
-        RemarkMathPlugin,
-      ],
-      renderers: {
-        ...props.renderers,
-        math: (props) => 
-          <MathJax.Node formula={props.value} />,
-        inlineMath: (props) =>
-          <MathJax.Node inline formula={props.value} />
-      }
-    };
-    return (
-      <MathJax.Provider input="tex">
-          <ReactMarkdown {...newProps} />
-      </MathJax.Provider>
-    );
+    ...props,
+    plugins: [
+      RemarkMathPlugin,
+    ],
+    renderers: {
+      ...props.renderers,
+      math: (props) => <MathJax.Node formula={props.value} />,
+      inlineMath: (props) => <MathJax.Node inline formula={props.value} />
+    }
+  }
+  return (
+    <MathJax.Provider input="tex">
+        <ReactMarkdown {...newProps} />
+    </MathJax.Provider>
+  )
 }
 
 function SheetContainer(){
   const {sheetFile:fileLocation} = React.useContext(CourseContext)
   const [fileString,setFileString] = React.useState('intro.md')
+
   React.useEffect(() => {
     fetch(`.\\chapters\\${fileLocation}`).then(response => response.text())
       .then(response => setFileString(response))
