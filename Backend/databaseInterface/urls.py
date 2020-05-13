156abc9147e428b03a48f7ed26f386f37ab3032f
ThomasShih <55861476+ThomasShih@users.noreply.github.com>
2020-05-13 11:28:41 -0600

    Fully set up django API responses
    TODO: Setup 404 error codes

--- Backend/databaseInterface/urls.py
+++ Backend/databaseInterface/urls.py
@@ -5,6 +5,5 @@ from . import views
 urlpatterns = [
   path('', views.availableCourses),
   path('course/<str:subject>/<int:code>/',views.getCourse),
-  path('chapter/<int:key>/',views.getChapter),
-  path('chapter/<int:key>/md',views.getChapterMD),
+  path('md/<int:key>/',views.getChapterMD),
 ]
