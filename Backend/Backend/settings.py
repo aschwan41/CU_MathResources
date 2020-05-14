f2bfa0a67f70cb7fe00bc2acd4523d207f8ebbc3
ThomasShih <55861476+ThomasShih@users.noreply.github.com>
2020-05-14 13:04:08 -0600

    Added CORS to whitelist

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['thomasshih.pythonanywhere.com']
 
 INSTALLED_APPS = [
     'databaseInterface.apps.DatabaseinterfaceConfig',
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -43,6 +44,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -119,3 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_WHITELIST = [
+    "https://cumathnotes.netlify.app",
+    "http://localhost:1234",
+]
